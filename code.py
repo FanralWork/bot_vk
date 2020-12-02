071230a39c857f1bdee8479eb4a33b1969900ccc
Fanral_Python <73023328+FanralWork@users.noreply.github.com>
2020-12-02 17:19:41 +0300

    1232545645646126512+52+

--- code.py
+++ code.py
@@ -1,13 +1,53 @@
 import vk_api, json
+from telebot.apihelper import send_message
+from vk_api.keyboard import VkKeyboard
 from vk_api.longpoll import VkLongPoll, VkEventType
 from config import main_token
 from bad_words import words
+from vk_api import keyboard
 
 vk_session = vk_api.VkApi(token = main_token)
 longpoll = VkLongPoll(vk_session)
 
+keyboard = {
+    "one_time": None,
+    "buttons": [
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{}",
+                 "label": "Вопросы 🤷"
+            },
+            "color": "positive"
+        }],
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{}",
+                "label": "Пригласть 🖐"
+            },
+            "color": "default"
+        },
+        {
+            "action": {
+                "type": "text",
+                "payload": "{}",
+                "label": "Развлечения 🎉"
+            },
+            "color": "default"
+            }
+
+
+        ]
+    ]
+}
+
+
+keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
+keyboard = str(keyboard.decode('utf-8'))
+
 def sender(id,text):
-    vk_session.method('messages.send', {'chat_id' : id, 'message' : text, 'random_id' : 0})
+    vk_session.method('messages.send', {'chat_id' : id, 'message' : text, 'random_id' : 0, 'keyboard' : keyboard})
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
@@ -21,9 +61,7 @@ for event in longpoll.listen():
                 if msg == 'привет':
                     sender(id, "Здравствуй, кванторианец")
 
-                if msg == 'где находится кванториум?':
-                    sender(id, "«Детский технопарк Кванториум Дружба» находится по адресу Россия, Рязань, улица Дзержинского, 6.")
 
-                if msg in words:
+                elif msg in words:
                     sender(id, f'@{event.user_id}, вы нарушили правило беседы!')
 
